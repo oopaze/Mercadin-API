1e8c45ee9dbe42098061e3039c500914fdc8b0b4
Jose Pedro <pedroosd28@gmail.com>
2020-08-22 23:30:49 -0300

    ''

--- app/routes/employees.py
+++
@@ -1,4 +0,0 @@
-from flask import Blueprint
-from app.models.employees import Employees
-
-emp = Blueprint('employees', __name__)
--- app/routes/sales.py
+++
@@ -1,4 +0,0 @@
-from flask import Blueprint
-from app.models.sales import Sales
-
-sale = Blueprint('sales', __name__)
