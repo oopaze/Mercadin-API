283ad24fc0defba467f7b1e5be2d2a468361f14d
Jose Pedro <pedroosd28@gmail.com>
2020-08-25 03:17:35 -0300

    agoora vai

--- config.py
+++ config.py
@@ -3,16 +3,18 @@ import os
 class Config(object):
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	SECRET_KEY = 'MinhaNamoradaEUmaGostosinhaLinda'
+	basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 
 class Development(Config):
 	ENV = 'Development'
 	DEBUG = True
-	basedir = os.path.abspath(os.path.dirname(__file__))
 	SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'mercadin.db')
 
 
 class Production(Config):
 	ENV = 'Production'
 	DEBUG = False
+	os.environ['FLASK_APP'] = os.path.join(basedir, 'run.py')
 	SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'mercadin.db')
